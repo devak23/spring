bcf5b278606a2e8442fea01cce27502ace2c253d
devak23 <developer.ak23@gmail.com>
2017-08-21 21:23:05 +0530

    added produces="application/json" to the attribute list

--- employee-crud/src/main/java/com/ak/learning/employee/ws/v1/EmployeeWS.java
+++ employee-crud/src/main/java/com/ak/learning/employee/ws/v1/EmployeeWS.java
@@ -19,19 +19,19 @@ public class EmployeeWS {
     @Autowired
     private EmployeeService employeeService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<Collection<Employee>> getEmployees() {
         Collection<Employee> employees = employeeService.findAll();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/id={id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/id={id}", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<Employee> getEmployee(@PathVariable("id") long employeeId) {
         Employee employee = employeeService.findById(employeeId);
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/name={name}", method = RequestMethod.GET)
+    @RequestMapping(value = "/name={name}", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<List<Employee>> getEmployeesByName(@PathVariable("name") String name) {
         List<Employee> employees = employeeService.getEmployeesByName(name);
         return new ResponseEntity<>(employees, HttpStatus.OK);
