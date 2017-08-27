082b649832a88ffebf26c60ecf51d3f0d9e2943d
devak23 <developer.ak23@gmail.com>
2017-08-27 08:42:42 +0530

    Refactored code

--- employee-crud/src/main/java/com/ak/learning/employee/ws/v1/EmployeeWS.java
+++ employee-crud/src/main/java/com/ak/learning/employee/ws/v1/EmployeeWS.java
@@ -5,27 +5,24 @@ import com.ak.learning.employee.services.v1.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.List;
 
-@Controller
-@RequestMapping("employees")
+@RestController
+@RequestMapping("/rest/v1/employees")
 public class EmployeeWS {
     @Autowired
     private EmployeeService employeeService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+    @GetMapping
     public ResponseEntity<Collection<Employee>> getEmployees() {
         Collection<Employee> employees = employeeService.findAll();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/id={id}", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<Employee> getEmployee(@PathVariable("id") long employeeId) {
         Employee employee = employeeService.findById(employeeId);
         return new ResponseEntity<>(employee, HttpStatus.OK);
